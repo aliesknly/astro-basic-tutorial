3f738d7cd3083b3fe3ec166db29093d76adf4499
Aliesky Perez <aliesky.perez@gmail.com>
2023-12-31 16:42:18 -0500

    added react to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://profound-toffee-d6489b.netlify.app"
+  site: "https://profound-toffee-d6489b.netlify.app",
+  integrations: [preact()]
 });
